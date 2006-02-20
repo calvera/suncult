f6309bb5ba7da5d3a637c6e4831be8c12a8e4821
Karel Sommer <karel.sommer@gmail.com>
2006-02-20 15:17:29 +0000

    changed to rsync

--- suncult/post.sh
+++ suncult/post.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-scp -vpCr `find . -name '*' | grep -v CVS` calvera@shell.sourceforge.net:/home/groups/s/su/suncult/htdocs
+#scp -vpCr `find . -name '*' | grep -v CVS` calvera@shell.sourceforge.net:/home/groups/s/su/suncult/htdocs
+RSYNC_RSH=ssh rsync -aCv ./ calvera@shell.sourceforge.net:/home/groups/s/su/suncult/htdocs/
