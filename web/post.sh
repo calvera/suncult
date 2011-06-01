41ee68b598f6e23bfcd39a5e6cb9bd0d78c2c611
Rob Norris <robbieonsea@users.sourceforge.net>
2011-06-01 19:43:07 +0000

    New images for popup and display configuration.

--- web/post.sh
+++ web/post.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 #scp -vpCr `find . -name '*' | grep -v CVS` calvera@shell.sourceforge.net:/home/groups/s/su/suncult/htdocs
-RSYNC_RSH=ssh rsync -aCv ./ calvera@web.sourceforge.net:/home/groups/s/su/suncult/htdocs/
+if [ $USER == "rob" ]; then
+	SF_USER=robbieonsea
+else
+	SF_USER=calvera
+fi
+RSYNC_RSH=ssh rsync -aCv ./ $SF_USER@web.sourceforge.net:/home/groups/s/su/suncult/htdocs/
